9d1e33db163473583c2cb66fcd8c044b050169a2
Harris <hyango2002@yahoo.com>
2024-02-18 12:48:47 -0600

    changed bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "3-tier-arch"
+    bucket = "3-tier-arch-bucket"
     key    = "state/terraform.tfstate"
     region = "us-east-1"
     workspace_key_prefix  = "env"
